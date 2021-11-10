488f0214fb5e5c17d6ff1e0f9958baa04fef0e53
kr0nverk <32976107+kr0nverk@users.noreply.github.com>
2021-11-10 15:34:54 +0300

    Create MapReduce1.py

---
+++ MapReduce1.py
@@ -0,0 +1,17 @@
+import time
+
+def find_longest_string(list_of_strings):
+    longest_string = None
+    longest_string_len = 0
+    for s in list_of_strings:
+        if len(s) > longest_string_len:
+            longest_string_len = len(s)
+            longest_string = s
+    return longest_string
+
+if __name__ == '__main__':
+    list_of_strings = ['abc', 'python', 'dima']
+    large_list_of_strings = list_of_strings * 10000000
+
+    print(find_longest_string(large_list_of_strings))
+    print("--- %s seconds ---" % time.process_time())
